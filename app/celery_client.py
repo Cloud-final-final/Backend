c03daf9d13ce98a4b6e8e6dd216597d8a5edcf1a
Daniel Fuentes <d.fuentese@uniandes.edu.co>
2025-04-08 09:53:22 -0500

    .

--- app/celery_client.py
+++
@@ -1,6 +0,0 @@
-from celery import Celery
-import os
-
-REDIS_BROKER_URL = os.getenv("REDIS_BROKER_URL", "redis://10.128.0.4:6379/0")  # IP interna de Worker donde Redis corre
-
-celery = Celery('tasks', broker=REDIS_BROKER_URL)
